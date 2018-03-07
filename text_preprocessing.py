30be8e1abb43eee78bdf90d4fc153180872985aa
Charles Gallay <charles.gallay@epfl.ch>
2018-03-07 22:20:45 +0100

    Charles: adding the stopwords

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -5,7 +5,20 @@ import math
 import warnings
 import re
 
-stopwords = set(['an', 'a', 'the', '-PRON-'])
+stopwords = set(['i', 'me', 'my', 'myself', 'we', 'our','I',
+               'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your',
+               'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her',
+               'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs',
+               'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those',
+               'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do',
+               'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+               'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through',
+               'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on',
+               'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where',
+               'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such',
+                'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will',
+               'just','should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y',
+                'ma', '-PRON-'])
 
 class Preprocessor():
     def __init__(self, max_nb_words = 10000):
