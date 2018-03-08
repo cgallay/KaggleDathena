60e533e95d52ab30d027d0c9534d71c0ad17940c
Charles Gallay <charles.gallay@epfl.ch>
2018-03-08 19:15:28 +0100

    Charles: text summarization using textRank

--- text_summarization.py
+++ text_summarization.py
@@ -1,17 +1,28 @@
 import spacy
+import sumy
 
-def apply(text, interest):
-    """ Return: the imortant sentence of the text """
-    #sents = sent_tokenize(text)
-    #inter_sent = [sent for sent in sents if interesting_sent(sent)]
-    return get_surroundning(text, interest, 20)
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.text_rank import TextRankSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
 
-def interesting_sent(sent):
-    #NER = ne_chunk(pos_tag(word_tokenize(sent)))
-    return 'keppel' in sent.lower() or 'prudential' in sent.lower()
-
-def get_surroundning(text, word, nb_words):
-    nlp = spacy.load('en_core_web_sm') #TODO speed up by putting that in an object constuctor
-    doc = nlp(text) #apply the preprcessing pipline
-    sur = [doc[(i-nb_words):i+nb_words+1].text for (i, w) in enumerate(doc) if w.text.lower() == word.lower()]
-    return sur
+def apply(text, interest, top_k=5):
+    """
+    Return and tuple of list of tuple.
+        The first list contains all the sentence about the interest and there corresponding weigth in the document
+        The second list contains the top_k sentences of the document
+    """
+    LANGUAGE = "english"
+    parser =  PlaintextParser(text, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+    sent_importance = summarizer.rate_sentences(parser.document)
+    interesting_sent = []
+    for sent in sent_importance:
+        if interest.lower() in sent._text.lower():
+            interesting_sent.append((sent._text, sent_importance[sent]))
+    top_sent = summarizer(parser.document, top_k)
+    top_sent = [(s._text, sent_importance[s]) for s in top_sent]
+    return (interesting_sent, top_sent)
