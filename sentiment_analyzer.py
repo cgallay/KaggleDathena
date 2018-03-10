25126fe668ea460fe8990b95acd33146a37d4161
Charles Gallay <charles.gallay@epfl.ch>
2018-03-10 13:00:07 +0100

    Charles: Amazon dataset load speed up

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -11,9 +11,10 @@ from keras.callbacks import TensorBoard
 import text_preprocessing
 import util
 import numpy as np
+from nltk.corpus import sentiwordnet as swn
 
+datasets = ['Amazon', 'IMDB']
 class SentimentAnalyzer():
-    datasets = ['Amazon', 'IMDB']
     nb_lines_amazon = 100
     max_sent_length = 1600
     def __init__(self, model_path=None):
@@ -25,7 +26,7 @@ class SentimentAnalyzer():
             #model need to be trained
             pass
     def train(self, dataset='Amazon', model_path='models/my_model.h5',epochs=1, top_words=10000):
-        assert dataset in self.datasets, 'Dataset should be in that list ' + str(self.datasets)
+        assert dataset in datasets, 'Dataset should be in that list ' + str(datasets)
         if dataset == 'Amazon':
             X_train, y_train = load_dataset('dataset/amazonreviews/data', self.nb_lines_amazon)
         else:
@@ -80,6 +81,25 @@ class SentimentAnalyzer():
         #print(model.predict_classes(l_pred))
         return self.model.predict_proba(l_pred)
 
+class sentiwordnetAnalyzer():
+    nb_lines_amazon = 100 #dont need to be as big as for the CNN as this model is much simpler
+    def __init__(self, model_path=None):
+        word_index = util.load('safe/vocab_gensim.p') #util.load('safe/dico.p') #imdb.get_word_index()
+        self.preprocessor = text_preprocessing.Preprocessor(word_index)
+
+    def train(self, dataset = "Amazon", top_words=10000):
+        """Once the CNN is train we can train a model on top of this one.
+        A simple logistic regression that have also access to the senti wordnet dico    
+        """
+        assert dataset in datasets, 'Dataset should be in that list ' + str(datasets)
+        if dataset == 'Amazon':
+            X_train, y_train = load_dataset('dataset/amazonreviews/data', self.nb_lines_amazon)
+        else:
+            (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+            raise Exception('Dead code... This should be retest again')
+
+               
+
 def read_line(line):
     label = line[0:11]
     text = line[11:]
@@ -88,6 +108,9 @@ def read_line(line):
 
 def load_dataset(fname, nb_lines):
     """Load the Amazon dataset"""
+    import os.path
+    if os.path.isfile('safe/Amazon-'+str(nb_lines)+'.p'):
+        return util.load('safe/Amazon-'+str(nb_lines)+'.p')
     count = 1
     X = []
     y = []
@@ -105,4 +128,5 @@ def load_dataset(fname, nb_lines):
     dico = util.load('safe/vocab_gensim.p')
     preprocessor = text_preprocessing.Preprocessor(dico=dico) #TODO use the good number of max_word
     X = preprocessor.preprocess(X)
+    util.save((X,y), 'safe/Amazon-'+str(nb_lines)+'.p')
     return (X, y)
