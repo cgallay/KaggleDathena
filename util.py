e627557b89a5ea916a291052f93a1aedc745f64e
Charles Gallay <charles.gallay@epfl.ch>
2018-03-10 11:30:25 +0100

    Charles: python doc

--- util.py
+++ util.py
@@ -1,6 +1,17 @@
 import pickle
 def save(obj, name):
+    """
+    args:
+        obj: Object to save on the disc
+        name: path where to save the object
+    """
     pickle.dump(obj, open( name, "wb" ))
 
 def load(name):
+    """
+    args:
+        name: path where the object is located
+    Return:
+        Object pickle loaded from pathe name
+    """
     return pickle.load(open( name, "rb" ))
